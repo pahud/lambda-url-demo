851a6b7dda0eccbc159147801b9beae32a1fc01b
Pahud Hsieh <pahudnet@gmail.com>
2022-07-28 16:22:02 +0000

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  App, Stack, StackProps, CfnOutput,
+  aws_lambda as lambda,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+
+    const handler = new lambda.Function(this, 'handler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: new lambda.InlineCode(`
+    exports.handler = async function(event, context) {
+      return {
+        statusCode: 200,
+        headers: { "content-type": "text/html" },
+        body: '<h1>Hello Hackathon!</h1>',
+      };
+    };`),
+    });
+
+    const fnUrl = handler.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new CfnOutput(this, 'URL', { value: fnUrl.url });
 
     // define resources here...
   }
